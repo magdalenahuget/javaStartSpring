71fd6e02bb0cffc29e9ef3d72d63003ba7506f72
magdalenahuget <magdalena.huget@gmail.com>
2023-10-03 16:06:10 +0200

    Add profile

--- src/main/java/pl/javastart/di/FileService.java
+++ src/main/java/pl/javastart/di/FileService.java
@@ -2,6 +2,7 @@ package pl.javastart.di;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import pl.javastart.di.crypto.CipherService;
 
 import java.io.BufferedWriter;
 import java.io.FileWriter;
@@ -14,9 +15,11 @@ import java.util.stream.Collectors;
 @Service
 class FileService {
     private final String fileName;
+    private final CipherService cipherService;
 
-    public FileService(@Value("${app.filename}") String fileName) {
+    public FileService(@Value("${app.filename}") String fileName, CipherService cipherService) {
         this.fileName = fileName;
+        this.cipherService = cipherService;
     }
 
     List<Entry> readAllFile() throws IOException {
